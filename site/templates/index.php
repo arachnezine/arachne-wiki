7f3f700dcfffa8c334c7d8c40d6dfa61ff9ec1b6
afincato <work@andrefincato.info>
2018-02-15 18:14:39 +0100

    add H:m:s to article timestamp

--- site/templates/index.php
+++ site/templates/index.php
@@ -1,18 +1,16 @@
-<?php if(!$site->user()) go('/') ?>
 <?php snippet('header') ?>
 
   <main class="" role="main">
-   <h1><?= $page->title()->html() ?></h1>
-   <div class="">
+    <h1><?= $page->title()->html() ?></h1>
+    <div class="">
       <?= $page->text()->kt() ?>
     </div>
 
     <ul>
     <?php foreach($page->children()->visible() as $article): ?>
-      <li><a href="<?= $article->url() ?>"><?= $article->title() ?>, <?= $article->modified('d-m-Y') ?></a></li>
+      <li><a href="<?= $article->url() ?>"><?= $article->title() ?>, <?= $article->modified('d-m-Y H:m:s') ?></a></li>
     <?php endforeach ?>
     </ul>
-
   </main>
 
 <?php snippet('footer') ?>
